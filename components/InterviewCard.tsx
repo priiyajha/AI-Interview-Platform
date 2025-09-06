4d63ded1fcd5a42a37528a9ed4e5639c54fdff53
priiyajha <jhapriiyaa2104@gmail.com>
2025-09-06 15:01:24 +0530

    added live interview ui

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import DisplayTechIcons from "@/components/DisplayTechIcons";
 
 
-const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}:InterviewCardProps) => {
+const InterviewCard = ({id, userId, role, type, techstack, createdAt}:InterviewCardProps) => {
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type)? 'Mixed': type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt||
@@ -42,8 +42,8 @@ const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}:I
                     <DisplayTechIcons techStack={techstack}/>
                     <Button className="btn-primary">
                         <Link href={feedback
-                            ?`/interview/${interviewId}/feedback`
-                            :`/interview/${interviewId}`}>
+                            ?`/interview/${id}/feedback`
+                            :`/interview/${id}`}>
                             {feedback? 'Check Feedback' : 'View Interview'}
                         </Link>
                     </Button>
