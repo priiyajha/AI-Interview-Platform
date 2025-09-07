a77b5c43b76966a6c56a859b68fa05591c0999da
priiyajha <jhapriiyaa2104@gmail.com>
2025-09-07 12:55:42 +0530

    Added feedback page ui

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,50 +1,10 @@
-import {db} from "@/firebase/admin";
-import {generateObject} from "ai";
-import {google} from "@ai-sdk/google";
-import {feedbackSchema} from "@/constants";
+"use server";
 
-export async function getInterviewByUserId(userId: string): Promise<Interview[] | null> {
-    const interviews = await db
-        .collection('interviews')
-        .where('userId','==', userId)
-        .orderBy('createdAt', 'desc')
-        .get();
+import { generateObject } from "ai";
+import { google } from "@ai-sdk/google";
 
-    return interviews.docs.map((doc)=>({
-        id: doc.id,
-        ...doc.data()
-    }))as Interview[];
-}
-
-
-export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
-    const {userId,limit=20} = params;
-
-    const interviews = await db
-        .collection('interviews')
-        .orderBy('createdAt', 'desc')
-        .where('finalized', '==', true)
-        .where('userId','==', userId)
-        .limit(limit)
-        .get();
-
-    return interviews.docs.map((doc)=>({
-        id: doc.id,
-        ...doc.data()
-    }))as Interview[];
-}
-
-
-
-export async function getInterviewById(id: string): Promise<Interview | null> {
-    const interview = await db
-        .collection('interviews')
-        .doc(id)
-        .get();
-
-    return interview.data() as Interview | null;
-}
-
+import { db } from "@/firebase/admin";
+import { feedbackSchema } from "@/constants";
 
 export async function createFeedback(params: CreateFeedbackParams) {
     const { interviewId, userId, transcript, feedbackId } = params;
@@ -57,7 +17,7 @@ export async function createFeedback(params: CreateFeedbackParams) {
             )
             .join("");
 
-        const { object } = await generateObject({
+        const { object } = await generateObject ({
             model: google("gemini-2.0-flash-001", {
                 structuredOutputs: false,
             }),
@@ -104,4 +64,62 @@ export async function createFeedback(params: CreateFeedbackParams) {
         console.error("Error saving feedback:", error);
         return { success: false };
     }
+}
+
+export async function getInterviewById(id: string): Promise<Interview | null> {
+    const interview = await db.collection("interviews").doc(id).get();
+
+    return interview.data() as Interview | null;
+}
+
+export async function getFeedbackByInterviewId(
+    params: GetFeedbackByInterviewIdParams
+): Promise<Feedback | null> {
+    const { interviewId, userId } = params;
+
+    const querySnapshot = await db
+        .collection("feedback")
+        .where("interviewId", "==", interviewId)
+        .where("userId", "==", userId)
+        .limit(1)
+        .get();
+
+    if (querySnapshot.empty) return null;
+
+    const feedbackDoc = querySnapshot.docs[0];
+    return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
+}
+
+export async function getLatestInterviews(
+    params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+    const { userId, limit = 20 } = params;
+
+    const interviews = await db
+        .collection("interviews")
+        .orderBy("createdAt", "desc")
+        .where("finalized", "==", true)
+        .where("userId", "!=", userId)
+        .limit(limit)
+        .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    })) as Interview[];
+}
+
+export async function getInterviewsByUserId(
+    userId: string
+): Promise<Interview[] | null> {
+    const interviews = await db
+        .collection("interviews")
+        .where("userId", "==", userId)
+        .orderBy("createdAt", "desc")
+        .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    })) as Interview[];
 }
